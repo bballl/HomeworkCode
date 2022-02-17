eeea88843c25cc65f4005cd0eaf14905229ab0e5
TikhonovGleb <bball2011@yandex.ru>
2022-02-17 12:15:20 +0300

    Homework1

---
+++ Homework.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppHelloWorld
+{
+    class Homework
+    {
+        static void Main(string[] args)
+        {
+            //задание 1
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+
+            //задание 2
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
